cc7452072626b73b8c73f073ec500615d9c79b1e
Anita West <missanitawest@gmail.com>
2022-05-18 21:57:10 -0700

    added tic

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,53 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+document.onkeydown = checkKey
+
+function checkKey(e) {
+  if (e.keyCode == '38') {
+    if (e.keyCode == '40') {
+    } else if (e.keyCode == '37') {
+      changeHorizontal(-1)
+    } else if (e.keyCode == '39') {
+      changeHorizontal(1)
+    }
+  }
+
+  var Indexes = [0, 0, 0]
+
+  var mainIndex = 0
+
+  var head = document.getElementById('head')
+  var head = document.getElementById('body')
+  var head = document.getElementById('shoes')
+
+  var imgs = [head, body, shoes]
+  var strings = ['head', 'body', 'shoes']
+
+  function changeHorizontal(shift) {
+    var index = indexes[mainIndex]
+    var image = imgs[mainIndex]
+    var str = strings[mainIndex]
+
+    Index += shift
+
+    if (index < 0) index = 5
+
+    if (index > 5) index = 0
+
+    indexes[mainIndex] = index
+
+    image.src = './images/' + str + index + 'png'
+  }
+
+  function changeVerticle(shift) {
+    mainIndex += shift
+
+    if (mainIndex < 0) mainIndex = 2
+
+    if (mainIndex > 2) mainIndex = 0
+  }
+}
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,29 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+var cells = document.getElementsByTagName('ID')
+
+var randomIndex = Math.floor(Math.random() * cells.length)
+
+var randomCell = cells[randomIndex]
+
+var mole = document.createElement('img')
+
+mole.src = './mole.PNG'
+mole.id = 'mole'
+
+randomCell.appendChild(mole)
+
+mole.onclick = whackedMole
+
+function whackedMole() {
+  randomIndex = Math.floor(Math.random() * cells.length)
+  randomCell = cells[randomIndex]
+  randomCell.appendChild(mole)
+
+  var audio = new Audio('./whack-audio.wav')
+  audio.play()
+}
